8a97e5757800fe03a2c213293f0cad2320fe0f3a
RVK6198 <rvk6198@gmail.com>
2024-04-18 02:53:19 +0530

    clerk authentication sign-in

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,13 @@
 // import { Button } from "@/components/ui/button";
 // import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      This is a protected route
+      <UserButton 
+        afterSignOutUrl="/"
+      />
     </div>
     
   );
